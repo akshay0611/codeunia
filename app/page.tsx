e7036d1a348d5f57d18c6d434cf806b5bbe94055
Akshay <akshay.allen26200@gmail.com>
2025-06-15 21:37:50 +0530

    Added Features Section/Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { HeroSection } from "@/components/home/HeroSection"
-
+import { FeaturesSection } from "@/components/home/FeaturesSection"
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center w-full overflow-hidden">
       <div className="w-full max-w-[2000px]">
         <HeroSection />
-       
+        <FeaturesSection />
       </div>
     </div>
   )
